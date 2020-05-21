749a1b502fce8feb2ab4e69a672de0e536b08c31
Joao Diego <jdflorencio@gmail.com>
2020-05-21 14:03:08 -0300

    homview

--- src/Modules/Modulos.js
+++ src/Modules/Modulos.js
@@ -3,25 +3,12 @@ import angular from 'angular'
 import {HomeComponent} from './home/Home'
 import HomeService from './home/HomeServices'
 
-import {GrupoComponent} from './grupo/Grupo'
-import GrupoService from './grupo/GrupoServices'
-
-import {SubGrupoComponent} from './subGrupo/SubGrupo'
-import SubGrupoService from './subGrupo/SubGrupoServices'
-
 import SubModulos from './subModules/SubModulos'
-
 const Modulos = 'modulos'
-
 angular.module(Modulos, [
     SubModulos,
-    HomeService,
-    GrupoService,
-    SubGrupoService
+    HomeService
+    
 ])
-
 .component('home', HomeComponent)
-.component('grupo', GrupoComponent)
-.component('subgrupo', SubGrupoComponent)
-
 export default Modulos
--- src/Modules/home/Home.js
+++ src/Modules/home/Home.js
@@ -1,12 +1,53 @@
 import template from './Home.html'
 
-function HomeController($http, HomeService, $state, $scope) {
+function HomeController($http, HomeService, $state, $scope, $mdDialog) {
   self = this
-  
-  HomeService.getAll()
-  
 
+  HomeService.getAll()
+
+  self.showConfirm = function (ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    const confirm = $mdDialog.confirm()
+      .title('Deseja excluir?')
+      .textContent('Tem certeza que deseja apagar os itens selecionados?')
+      .ariaLabel('Lucky day')
+      .targetEvent(ev)
+      .ok('Excluir')
+      .cancel('Cancelar')
+
+    const alert = $mdDialog.alert()
+      .parent(angular.element(document.querySelector('#popupContainer')))
+      .clickOutsideToClose(true)
+      .title(':( Ops! Algo deu errado ')
+      .textContent('Selecione um item para excluir.')
+      .ariaLabel('Alert Dialog Demo')
+      .ok('Ok')
+      .targetEvent(ev)
+
+
+    const prompt = $mdDialog.prompt()
+      .title('Adicionar Nova Sprint?')
+      // .textContent('Insira o link da SPRINT')
+      .placeholder('Digite Aqui')
+      // .ariaLabel('Digite')
+      .placeholder('exemple? "https://trello.com"')
+      .targetEvent(ev)
+      .required(true)
+      .ok('ok')
+      .cancel('Cancelar');
+
+
+
+
+    $mdDialog.show(prompt).then(function (URL) {
+      $scope.status = 'You decided to get rid of your debt.';
   
+    }, function () {
+      $scope.status = 'You decided to keep your debt.';
+    });
+  };
+
+
 
 }
 
