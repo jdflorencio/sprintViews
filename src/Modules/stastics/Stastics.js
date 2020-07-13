900171d9f12cf810528115de0b1f7076ae93c00b
João Diego <jdflorencio@gmail.com>
2020-07-13 14:38:19 -0300

    Migrando para higchart

--- src/Modules/stastics/Stastics.js
+++ src/Modules/stastics/Stastics.js
@@ -2,102 +2,44 @@ import template from './Stastics.html'
 import {
   database
 } from '../../firebase'
-
-/* 
-class Controller {
-  constructor($stateParams) {
-    self = this
-    self.onload = true
-    self.$stateParams = $stateParams
-    self.options = 'joao'
-    console.log(self.$stateParams)
-  }
-
-  async firebasedata() {
-
-    let sprintData = database.ref(`scrum/sprints/` + self.$stateParams.id)
-
-    let sprintPayload = await sprintData.once('value')
-      .then(function (snapshot) {
-        return { status: true, data: snapshot.val() }
-      })
-      .catch(function (error) {
-        return { status: false, data: error }
-      })
-
-    let stasticsData = database.ref(`scrum/statics/` + self.$stateParams.id)
-
-    let stasticsPayload = await stasticsData.once('value')
-      .then(function (snapshot) {
-        return { status: true, data: snapshot.val() }
-      })
-      .catch(function (error) {
-        return { status: false, data: error }
-      })
-
-
-    return {
-      stasticsPayload,
-      sprintPayload
-    }
-  }
-}
-
-export default Controller */
-/* 
-async function StasticsController($state, $stateParams, $q) {
-  self = this
-  let deferred = $q.defer();
-  self.onload = true
-  deferred.resolve(retornoFireBase($stateParams, $q))
-  const values = deferred.promise.then(success).then(success)
-  console.log(values)
-
-} */
-
-
-/* class Base {
-  constructor($state, $stateParams) {
-    self = this
-    self.$state = $state
-    self.$stateParams = $stateParams
-    self.onload = true
-  }
-}
-class Teste extends Base {
-  constructor($state, $stateParams) {
-    self = this
-
-    self.options = {}
-    self.$stateParams = $stateParams
-    self.$state = $state
-    self.template = template
-
-  }
-p
-} */
+import { data } from 'autoprefixer'
 
 function stasticsController($state, $stateParams, $firebaseArray, $firebaseObject) {
 
     self = this
-
-    self.teste = {
-      labels: ["2014", "2013", "2012", "2011"],
-      datasets: [[727, 589, 537, 10]]
-    }
-
-    self.labelPontos = ["Média Dev", "Pontos"]
-
-    self.media = []
-
-    const sprint = database.ref(`scrum/sprints/` + $stateParams.id) 
-
-
+    
+    const sprint = database.ref(`scrum/sprints/` + $stateParams.id)
     let sprintInfor = $firebaseObject(sprint)
     sprintInfor.$loaded().then(res => {
-      self.media.push(res.mediaDev)
-      self.media.push(res.pesoTotal)
 
+      self.chart1 = {
+        chart: {
+          renderTo: 'container',
+          type: 'pie',
+          height: 300,
+          width: 300,
+          backgroundColor: null,
+        },
+        yAxis: {
+          title: {
+            text: ''
+          }
+        },
+        title: {
+          text: 'Pontos Obtidos',
+        },
+        legend: {
+          itemDistance: 50,
+        },
+  
+        series: [{
+          innerSize: '65%',
+          data: [
+            { name: "Média Dev", y: res.mediaDev },
+            { name: "Pontos", y: res.pesoTotal }  
+          ]
+        }]
+      }
     })
 
     self.totalDev = {
@@ -117,11 +59,13 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
       valores: []
     }
 
-    const totalPordev = $firebaseArray(database.ref(`scrum/stastics/` + $stateParams.id))
-    totalPordev.$loaded().then((res) => {
-
+    const totalPordev = $firebaseArray(database.ref(`scrum/statics/` + $stateParams.id))
+    totalPordev.$loaded().then(res => {
+      
       const por_label = []
       const legenda = []
+      const data2 = []
+      const data3 =[]
 
       const totalMelhoria = res.reduce((total, element) => {
         return total + (element.melhoria ? element.melhoria : 0)
@@ -143,24 +87,34 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
         return total + element.total_pontos_dev
       }, 0)
 
+    /*   console.table([{name: 'totalMelhoria', valor: totalMelhoria },
+          {name: 'totalBug', valor: totalBug },
+          {name: 'totalDebito_tecnico', valor: totalDebito_tecnico },
+          {name: 'totalImplemetacao', valor: totalImplemetacao }
+    ]) */
+
       if (totalMelhoria != 0) {
         por_label.push(totalMelhoria)
         legenda.push('Melhoria')
+        data2.push({name: "Melhoria", y: totalMelhoria})
       }
 
       if (totalBug != 0) {
         por_label.push(totalBug)
         legenda.push('Bug')
+        data2.push({name: "bug", y: totalBug})
       }
 
       if (totalDebito_tecnico != 0) {
         por_label.push(totalDebito_tecnico)
         legenda.push('Debito Técnico')
+        data2.push({name: "Debito Técnico", y: totalDebito_tecnico})
       }
 
       if (totalImplemetacao != 0) {
         por_label.push(totalImplemetacao)
         legenda.push("Implmentação")
+        data2.push({name: "Implementação", y: totalImplemetacao})
       }
 
       self.totalDev.por_label = por_label
@@ -170,14 +124,18 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
 
       if (totalPontosSomandos < totalDePontos) {
         por_label.push((totalDePontos - totalPontosSomandos))
+        const totalOutros = (totalDePontos - totalPontosSomandos)
         self.totalDev.legenda.push("Outros")
+        data2.push({name: "Outros", y: totalOutros})
       }
 
+      
       /** GRAFICO 3 **/
       res.forEach(result => {
-        self.grafico3.label.push(result.nome.split(' ')[0])
-        self.grafico3.valores.push(result.total_pontos_dev)
+        data3.push({name: result.nome.split(' ')[0] ,data: [result.total_pontos_dev]})
       })
+
+      console.log(data3)
 
       /** GRAFICO 4 **/
 
@@ -188,129 +146,42 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
       const debito_tecnico = []
 
       res.forEach(result => {
-
         self.grafico4.label.push(result.nome.split(' ')[0])
-
         melhorias.push(result.melhoria || 0)
         implementacao.push(result.implementacao || 0)
         debito_tecnico.push(result.debito_tecnico || 0)
         bugs.push(result.bug || 0)
-
         /* outros_nao_atribuidos.push(outros || 0) */
-
       })
 
       self.grafico4.valores.push(melhorias)
       self.grafico4.valores.push(debito_tecnico)
       self.grafico4.valores.push(implementacao)
       self.grafico4.valores.push(bugs)
+
       /* self.grafico4.valores.push(outros_nao_atribuidos) */
-    })
-
-    self.chart2 = {
-      values: [],
-      labels: []
-    }
-
-    self.situacao = {
-      backlog: false,
-      andamento: true,
-      concluido: false
-    }
-
-    self.irPara = function (stastics) {
-      console.log(`estou no ${stastics}`)
-    }
-
-    self.back = function () {
-      $state.go('home')
-    }
-
-    self.chart = {
-      chart: {
-        renderTo: 'container',
-        type: 'pie',
-        height: 300,
-        width: 300,
-        backgroundColor: null,
-      },
-      legend: {
-        itemDistance: 50,
-      },
-
-      series: [{
-        innerSize: '65%',
-        data: [
-          { name: 'Yellow Slice', y: 12 },
-          { name: 'Red Slice', y: 88 },
-          /* {name: 'Red Slice', y: 88, color: 'red' }, */
-
-        ]
-      }]
-    }
 
     self.chart2 = {
       chart: {
         plotBackgroundColor: null,
-        /*   plotBorderWidth: null,
-          plotShadow: false, */
+    
         type: 'pie',
         height: 300,
         width: 300,
       },
-      title: {
-        text: 'Browser market shares in January, 2018'
+      yAxis: {
+        title: {
+          text: ''
+        }
       },
-
-      /*  legend: {
-         reversed: true
-       }, */
-      /* tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      }, */
-
-      // accessibility: {
-      //   point: {
-      //     valueSuffix: '%'
-      //   }
-      // },
-      /*   plotOptions: {
-          pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-              enabled: false
-            },
-            //showInLegend: true,
-          }
-        }, */
-      /*   series: [{
-          name: 'Brands',
-          colorByPoint: true,
-          data: [{
-            name: 'Chrome',
-            y: 61.41,
-            sliced: true,
-            selected: true
-          }, {
-            name: 'Internet Explorer',
-            y: 11.84
-          }, {
-            name: 'Firefox',
-            y: 10.85
-          }]
-        }] */
-
+      title: {
+        text: 'O que foi desenvolvido',
+      },
       series: [{
-        data: [
-          { name: 'Yellow Slice', y: 12 },
-          { name: 'Red Slice', y: 88 },
-          { name: 'Red Slice', y: 88 },
-          { name: 'Red Slice', y: 88 },
-        ]
+        data: data2
       }]
     }
-
+    })
 
     self.chart3 = {
       chart: {
@@ -319,14 +190,12 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
         width: 300,
         backgroundColor: null,
       },
-
       title: {
-        text: 'dfsadfsdfsdf',
+        text: ' Produção do dev',
       },
       legend: {
         reversed: true
       },
-
       xAxis: {
         labels: {
           x: -10,
@@ -341,11 +210,12 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
         gridLineWidth: 0,
         minorGridLineWidth: 0,
         title: {
-          text: 'Amount'
+          text: ''
         }
       },
 
-      series: [{
+      series: [
+        {
         name: 'backlog',
         data: [4]
       }, {
@@ -363,38 +233,7 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
       }, {
         name: 'Concluído',
         data: [8]
-      }],
-
-      responsive: {
-        rules: [{
-          condition: {
-            maxWidth: 600
-          },
-          chartOptions: {
-            legend: {
-              align: 'center',
-              verticalAlign: 'bottom',
-              layout: 'horizontal'
-            },
-            yAxis: {
-              labels: {
-                align: 'left',
-                x: 0,
-                y: -5
-              },
-              title: {
-                text: null
-              }
-            },
-            subtitle: {
-              text: null
-            },
-            credits: {
-              enabled: false
-            }
-          }
-        }]
-      }
+      }]
     }
 
     self.chart4 = {
@@ -404,16 +243,13 @@ function stasticsController($state, $stateParams, $firebaseArray, $firebaseObjec
         width: 300,
       },
       title: {
-        text: 'Stacked bar chart'
+        text: 'Produção do dev - Tipo'
       },
       xAxis: {
         categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
       },
       yAxis: {
-        min: 0,
-        /* title: {
-          text: 'Total fruit consumption'
-        } */
+        min: 0
       },
       legend: {
         reversed: true
